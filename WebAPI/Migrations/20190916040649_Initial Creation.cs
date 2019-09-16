27964aeefcb3ca6dd88a6744c17f780a35ae4bbd
Andy Hung <hunga@mail.gvsu.edu>
2019-09-16 00:40:42 -0400

    Added the beginnings of the back-end Web API

---
+++ WebAPI/Migrations/20190916040649_Initial Creation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    OrdId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    CustomerPhone = table.Column<string>(type: "nvarchar(9)", nullable: false),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    CVV = table.Column<string>(type: "nvarchar(3)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Order = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.OrdId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ WebAPI/Models/OrderDetail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrdId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string CustomerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(9)")]
+        public string CustomerPhone { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(3)")]
+        public string CVV { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string Location { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Order { get; set; }
+    }
+}
